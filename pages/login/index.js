973dfbcbbe7dc0ddb6ff548c8511177a4df2b86f
Roshan <roshanbudhathoki02@gmail.com>
2022-10-15 06:48:20 +1100

    add registration page

--- pages/login/index.js
+++ pages/login/index.js
@@ -1,15 +1,16 @@
 import React, { useState } from 'react';
 import { Card, Button, Form, Container } from 'react-bootstrap';
+import Link from 'next/link';
 function login() {
-    const [isRoleSelected, setRole] = useState(null);
+    const [role, setRole] = useState(null);
 
     
     return(
         <Container className="d-flex justify-content-center align-items-center">
-            <Card className="rounded p-4 mt-5" style={{width: "max-content"}}>
-            { isRoleSelected &&
+            <Card className="rounded py-4 px-5  mt-5" style={{width: "max-content"}}>
+            { role &&
                     <Form>
-                        <h3 className="mb-3">Login</h3>
+                        <h3 className="mb-3">{ role } Login</h3>
                         <Form.Group className="mb-3" controlId="formBasicEmail">
                             <Form.Label>Email address</Form.Label>
                             <Form.Control type="email" placeholder="Enter email" />
@@ -19,18 +20,25 @@ function login() {
                             <Form.Label>Password</Form.Label>
                             <Form.Control type="password" placeholder="Password" />
                         </Form.Group>
-                        <Button variant="primary" type="submit">
-                            Login
-                        </Button>
+                        <div>
+                            <Button variant="primary" type="submit">
+                                Login
+                            </Button>
+                            <span className="mx-4">
+                                <Link href='/registration' variant="primary">
+                                    Register
+                                </Link>
+                            </span>
+                        </div>
                     </Form>
             }
-            {!isRoleSelected && 
+            { !role && 
                 <div>
-                        <h3 className="m-5">Select Role</h3>
+                        <h3 className="mb-5">Select Role</h3>
                         <div>
-                            <Button onClick={event => setRole('teacher')}>Teacher</Button>
+                            <Button onClick={event => setRole('Teacher')}>Teacher</Button>
                             <p>or</p>
-                            <Button onClick={event => setRole('student')}>Student</Button>
+                            <Button onClick={event => setRole('Student')}>Student</Button>
                         </div>
                     </div>
 
