212e8ca7655f302f8b457ab40e1aedf82636586c
Roshan <roshanbudhathoki02@gmail.com>
2022-10-18 11:29:27 +1100

    added leaderboard page

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,4 @@
-import Head from "next/head";
 import "../styles/global.css";
-// import 'react-bootstrap/dist/react-bootstrap'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import React from "react"
 import { wrapper } from "../src/store"
@@ -10,15 +8,3 @@ const MyApp = ({ Component, pageProps}) => (
 )
 
 export default wrapper.withRedux(MyApp);
-
-// export default function MyApp({ Component, pageProps }) {
-//   return (
-//     <div>
-//       <Head>
-//         <meta charSet="utf-8" />
-//         <meta name="viewport" content="width=device-width, initial-scale=1" />
-//       </Head>
-//      <Component {...pageProps} />
-//    </div>
-//   )
-// }
