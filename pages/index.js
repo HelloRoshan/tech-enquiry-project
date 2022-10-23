387f899d65032b40d56f8cc9573018fb65218c7d
Roshan <roshanbudhathoki02@gmail.com>
2022-10-24 01:09:21 +1100

    add logout action

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,19 @@
-import React from 'react';
-import {Button} from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Button, Modal } from 'react-bootstrap';
 import Container from 'react-bootstrap/Container';
 import Link from 'next/link';
 
 function App() {
+  const [showLogoutModal, setLogoutModal] = useState(false);
+
+  const handleClose = () => setLogoutModal(false);
+  const handleShow = () => setLogoutModal(true);
+
+  const handleLogout = () => {
+    /* clear login */
+    window.location.pathname = '/login';
+  };
+
   return (
       <Container className='d-flex align-items-center justify-content-center text-center min-vh-100 m-auto p-auto'>
         <div className="d-grid gap-4">
@@ -20,6 +30,24 @@ function App() {
           <Button variant="primary" size="lg">
             Feedbacks
           </Button>
+          <Button variant="primary" size="lg" onClick={handleShow}>
+            Logout
+          </Button>
+
+          <Modal show={showLogoutModal} onHide={handleClose}>
+            <Modal.Header closeButton>
+              <Modal.Title>Confirm Action: <span className="text-danger">Logout</span></Modal.Title>
+            </Modal.Header>
+            <Modal.Body>Are you sure you want to logout?</Modal.Body>
+            <Modal.Footer>
+              <Button variant="secondary" onClick={handleClose}>
+                Close
+              </Button>
+              <Button variant="primary" onClick={handleLogout}>
+                Logout
+              </Button>
+            </Modal.Footer>
+          </Modal>
         </div>
       </Container>
   );
