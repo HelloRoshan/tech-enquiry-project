3ba0504c71e9d4195174b027e20e822835486c9f
Roshan <roshanbudhathoki02@gmail.com>
2022-10-27 00:23:49 +1100

    merge conflict fix 2

--- pages/levels/index.js
+++ pages/levels/index.js
@@ -9,31 +9,30 @@ function Levels() {
   return (
     
     <Container className='d-flex align-items-center justify-content-center text-center min-vh-100 m-auto p-auto'>
-    <div className="d-grid gap-4">
+      <div className="d-grid gap-4">
         <Card className='d-flex  justify-content-center p-4 rounded shadow-lg' style={{width: 400, height:400}}>
-        <h1>Select Level</h1>
-        <br></br>
-        
-        <Link href={{ pathname: '/category', query: { level: '1' } }} variant="primary">
-             <Button variant="primary" size="lg" className='mb-3'>
+          <h1 className='text-prime-2 mb-5'>Select Level</h1>
+          <Link href={{ pathname: '/category', query: { level: '1' } }} variant="primary">
+            <Button variant="primary" size="lg" className='mb-3'>
                 1
             </Button>
-            </Link>
-            <Link href={{ pathname: '/category', query: { level: '2' } }} variant="primary">
+          </Link>
+          <Link href={{ pathname: '/category', query: { level: '2' } }} variant="primary">
              <Button variant="primary" size="lg" className='mb-3'>
                 2
             </Button>
-            </Link>
-            <Link href={{ pathname: '/category', query: { level: '3' } }} variant="primary">
-             <Button variant="primary" size="lg" className='mb-3'>
+          </Link>
+          <Link href={{ pathname: '/category', query: { level: '3' } }} variant="primary">
+            <Button variant="primary" size="lg" className='mb-3'>
                 3
             </Button>
-            </Link>
-            
-            </Card>
-        </div>
-     
-      </Container>
+          </Link>
+          <Link href="/" variant="primary">
+            <Button variant="primary" size="lg" className="mt-5 ms-auto me-auto">Back</Button>
+          </Link>
+        </Card>
+      </div>
+    </Container>
   );
 }
 
